9bd844017695e24f774297ad3ecdfe949e09481d
Chike Chris <cchike2017@gmail.com>
2019-07-19 15:24:12 -0400

    added stlyed component to App.js file

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import "./App.css";
+
 import Characters from "./components/Character";
+import styled from "styled-components";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -9,11 +10,21 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const AppDiv = styled.div`
+    text-align: center;
+  `;
+  const H1Div = styled.h1`
+    color: #443e3e;
+    text-shadow: 1px 1px 5px #fff;
+    font-size: 50px;
+    font-weight: 700;
+  `;
+
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+    <AppDiv className="App">
+      <H1Div className="Header">React Wars</H1Div>
       <Characters />
-    </div>
+    </AppDiv>
   );
 };
 
